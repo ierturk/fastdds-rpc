f6ba0f1ecd090eeb9dbf13ad27455536cdb5a7e3
Ibrahim Erturk <me@erturk.me>
2025-04-05 21:26:04 +0300

    Added license to files in root

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+# Copyright (c) 2025, Ibrahim Erturk <me@erturk.me>, ErturkMe
+# Licensed under the BSD 3-Clause License.
+# See the LICENSE file in the project root for more information.
+
 # Variables
 BUILD_DIR = build
 START_ENV_SCRIPT = start_env_and_vscode.bat
--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025, Ibrahim Erturk <me@erturk.me>, ErturkMe
+# Licensed under the BSD 3-Clause License.
+# See the LICENSE file in the project root for more information.
+
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout
 from conan.tools.scm import Version
